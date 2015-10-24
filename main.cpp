eda431255c2db2c79f4f539bab0c5e37c90e7aab
didima7777 <didima7777@gmail.com>
2015-10-24 19:55:44 +0300

    add test

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 #define N 1024
 
-void *print_message_function( void *ptr );
-
+void *test_fft1( void *ptr );
+void *test_fft2( void *ptr );
 
 
 fftw_complex *in1, *out1;
@@ -24,44 +24,51 @@ main()
 
     /* Create independent threads each of which will execute function */
 
-     //iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
-     //iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+     //iret1 = pthread_create( &test_fft1, NULL, print_message_function, (void*) message1);
 
      /* Wait till threads are complete before main continues. Unless we  */
      /* wait we run the risk of executing an exit which will terminate   */
      /* the process and all threads before the threads have completed.   */
      int a;
-     printf("Load 1024 point for 1000 repeat thread fftw3 \n");
+     printf("Load 1024 points for 1000 repeat thread fftw3 \n");
 
      start = clock();
      for (int i=0;i<1000;i++) {
-          iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+          iret1 = pthread_create( &thread1, NULL, test_fft1, (void*) message1);
           pthread_join( thread1, NULL);
      };
      end = clock();
      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
      printf("time %lf \n",cpu_time_used);
 
-     printf("Load 1024 point for 1000 repeat only fftw3 \n");
+     printf("Load 1024 points for 1000 repeat only fftw3 \n");
      start = clock();
     for (int i=0;i<1000;i++) {
-         print_message_function(&a);
+         test_fft1(&a);
      };
      end = clock();
      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
      printf("time %lf \n",cpu_time_used);
 
+     printf("Load 1024 points thred fftw3 \n");
+     start = clock();
+     iret2 = pthread_create( &thread2, NULL, test_fft2, (void*) message2);
+     pthread_join( thread1, NULL);
+     end = clock();
+     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+     printf("time %lf \n",cpu_time_used);
 
      printf("Thread 1 returns: %d\n",iret1);
      printf("Thread 2 returns: %d\n",iret2);
      exit(0);
 };
 
-void *print_message_function( void *ptr )
+void *test_fft1( void *ptr )
 {
          fftw_complex *in, *out;
          fftw_plan p;
          
+
          in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
          out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
 
@@ -80,4 +87,28 @@ void *print_message_function( void *ptr )
          fftw_free(in); 
          fftw_free(out);
          //printf("finish \n");
+}
+
+void *test_fft2( void *ptr )
+{
+     
+
+     for (int i=0;i<1000;i++) {    
+         fftw_complex *in, *out;
+         fftw_plan p;
+         
+         in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+         out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+
+         for(int i=0;i<N;i++) in[i][0]=i,in[i][1]=0;
+
+         p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+         
+         fftw_execute(p); /* repeat as needed */
+         
+         fftw_destroy_plan(p);
+         fftw_free(in); 
+         fftw_free(out);
+     }         
+     
 }
